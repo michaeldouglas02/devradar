4e7800b04a72189044b74dc38282c77e7a6a7101
Michael Douglas <michaeldouglas02@gmail.com>
2020-01-17 18:05:56 -0400

    Adicionado socketio

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL:
+    "https://3333-b4a748c1-ba30-4773-b70b-bd8afc7ec1f3.ws-us02.gitpod.io/"
 });
 
 export default api;
